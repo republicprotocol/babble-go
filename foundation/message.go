cf191aa6dbd8a6c2bfba1bb952fa6d47685cfb89
Loong <loong@republicprotocol.com>
2018-09-03 21:46:33 +1000

    foundation: message docs

--- foundation/message.go
+++ foundation/message.go
@@ -1,5 +1,10 @@
 package foundation
 
+// A Message is a unit of data that can be broadcast throughout the network. A
+// outdated Message can be overwritten by broadcasting a newer Message with the
+// same `Key` but an incremented `Nonce`. Nodes in the network will discard the
+// lower `Nonce` Message in favour of the higher `Nonce` Message. A `Signature`
+// is used to verify the authenticity of the Message.
 type Message struct {
 	Nonce     uint64 `json:"nonce"`
 	Key       []byte `json:"key"`
