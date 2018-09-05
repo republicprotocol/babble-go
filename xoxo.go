e5f58029761222ff8e6048184924b0ee54a27b11
Loong <loong@republicprotocol.com>
2018-09-05 10:52:41 +1000

    xoxo: fixed import

--- xoxo.go
+++ xoxo.go
@@ -1,9 +1,9 @@
 package xoxo
 
 import (
-	"github.com/republicprotocol/xoxo-go/adapter/grpc"
-	"github.com/republicprotocol/xoxo-go/adapter/leveldb"
 	"github.com/republicprotocol/xoxo-go/core/gossip"
+	"github.com/republicprotocol/xoxo-go/driver/grpc"
+	"github.com/republicprotocol/xoxo-go/driver/leveldb"
 	"github.com/republicprotocol/xoxo-go/foundation"
 )
 
