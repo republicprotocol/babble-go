b9b33eecf3c1c4146c9664a53ed4c88c13e9f96b
Loong <loong@republicprotocol.com>
2018-09-08 13:23:25 +1000

    driver/grpc: docs for gRPC driver

--- driver/grpc/grpc.go
+++ driver/grpc/grpc.go
@@ -3,7 +3,6 @@ package grpc
 import (
 	"context"
 	"errors"
-	"fmt"
 	"net"
 	"sync"
 	"time"
@@ -18,9 +17,14 @@ import (
 // over a period of time.
 var ErrRateLimitExceeded = errors.New("rate limit exceeded")
 
-// Dial a net.Addr to create an insecure connection to a remote server at that
-// net.Addr. A context can be used to cancel or expire the pending connection. A
-// call to grpc.ClientConn.Close is required to free all allocated resources.
+// ErrMalformedTCPAddress is returned when a server cannot determine the TCP
+// address of a client.
+var ErrMalformedTCPAddress = errors.New("malformed tcp address")
+
+// Dial a `net.Addr` to create an insecure connection to a remote server at that
+// `net.Addr`. A `context.Context` can be used to cancel or expire the pending
+// connection. A call to `grpc.ClientConn.Close` is required to free all
+// allocated resources.
 func Dial(ctx context.Context, addr net.Addr) (*grpc.ClientConn, error) {
 	conn, err := grpc.DialContext(ctx, addr.String(), grpc.WithInsecure())
 	if err != nil {
@@ -29,9 +33,10 @@ func Dial(ctx context.Context, addr net.Addr) (*grpc.ClientConn, error) {
 	return conn, nil
 }
 
-// Backoff calling a function until the context.Context is done, or the function
-// returns a nil error.
-func Backoff(ctx context.Context, f func() error, maxBackoffDelay time.Duration) error {
+// Backoff calling the `f` function until the `context.Context` is done, or the
+// `f` function returns a nil error. The delay increases by 60% but will not
+// exceed beyond the `maxBackoffDelayInMs`.
+func Backoff(ctx context.Context, f func() error, maxBackoffDelayInMs time.Duration) error {
 	timeoutMs := time.Duration(1000)
 	for {
 		err := f()
@@ -44,8 +49,8 @@ func Backoff(ctx context.Context, f func() error, maxBackoffDelay time.Duration)
 			return err
 		case <-timer.C:
 			timeoutMs = time.Duration(float64(timeoutMs) * 1.6)
-			if timeoutMs > maxBackoffDelay {
-				timeoutMs = maxBackoffDelay
+			if timeoutMs > maxBackoffDelayInMs {
+				timeoutMs = maxBackoffDelayInMs
 			}
 		}
 	}
@@ -54,12 +59,15 @@ func Backoff(ctx context.Context, f func() error, maxBackoffDelay time.Duration)
 type client struct {
 }
 
-// NewClient returns an implementation of the gossip.Client interface that uses
-// gRPC to invoke RPCs.
+// NewClient returns an implementation of the `gossip.Client` interface that
+// uses gRPC to invoke RPCs.
 func NewClient(addr net.Addr) gossip.Client {
 	return &client{}
 }
 
+// Send a `message` to the `to` address. A `context.Context` can be used to
+// cancel or expire the request. The client will backoff the request with a
+// maximum delay of one minute.
 func (client *client) Send(ctx context.Context, to net.Addr, message foundation.Message) error {
 	conn, err := Dial(ctx, to)
 	if err != nil {
@@ -80,30 +88,34 @@ func (client *client) Send(ctx context.Context, to net.Addr, message foundation.
 	}, time.Minute)
 }
 
-// Service is a Service that implements a gRPC Service that accepts RPCs. It
-// delegates request to a gossip.Server.
+// Service implements a gRPC Service that accepts RPCs from clients. It
+// delegates requests to a `gossip.Server` after enforcing rate limits.
 type Service struct {
 	server gossip.Server
 
-	rate         time.Duration
 	rateLimitsMu *sync.Mutex
 	rateLimits   map[string]time.Time
+	rate         time.Duration
 }
 
+// NewService returns a Service that delegates requests to the `server` and uses
+// `rate` to enforce rate limits of all RPCs.
 func NewService(server gossip.Server, rate time.Duration) Service {
 	return Service{
 		server: server,
 
-		rate:         rate,
 		rateLimitsMu: new(sync.Mutex),
 		rateLimits:   make(map[string]time.Time),
+		rate:         rate,
 	}
 }
 
+// Register the service to a `grpc.Server`.
 func (service *Service) Register(server *grpc.Server) {
 	RegisterXoxoServiceServer(server, service)
 }
 
+// Broadcast implements the respective gRPC call.
 func (service *Service) Broadcast(ctx context.Context, request *BroadcastRequest) (*BroadcastResponse, error) {
 	if err := service.isRateLimited(ctx); err != nil {
 		return nil, err
@@ -120,15 +132,15 @@ func (service *Service) Broadcast(ctx context.Context, request *BroadcastRequest
 func (service *Service) isRateLimited(ctx context.Context) error {
 	client, ok := peer.FromContext(ctx)
 	if !ok {
-		return fmt.Errorf("fail to get peer from ctx")
+		return ErrMalformedTCPAddress
 	}
 	if client.Addr == net.Addr(nil) {
-		return fmt.Errorf("fail to get peer address")
+		return ErrMalformedTCPAddress
 	}
 
 	clientAddr, ok := client.Addr.(*net.TCPAddr)
 	if !ok {
-		return fmt.Errorf("fail to read peer TCP address")
+		return ErrMalformedTCPAddress
 	}
 	clientIP := clientAddr.IP.String()
 
